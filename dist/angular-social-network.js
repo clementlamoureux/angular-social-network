ce6118aa83774ba432684a0c3e09be4c93e08c05
Clément LAMOUREUX <clement@raccourci.fr>
2014-09-23 12:10:53 +0200

    added dist & bower

---
+++ dist/angular-social-network.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('drupalAngularApp.components.rc-social-network', [])
+  .directive('rcSocialNetworkTwitter', function () {
+
+    return {
+      link: function (scope) {
+        var idScript;
+        window.twttr = (function (d, s, id) {
+          idScript = id;
+          var t, js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {
+            return;
+          }
+          js = d.createElement(s);
+          js.id = id;
+          js.src = 'https://platform.twitter.com/widgets.js';
+          fjs.parentNode.insertBefore(js, fjs);
+          return window.twttr || (t = {_e: [], ready: function (f) {
+            t._e.push(f);
+          }});
+        }(document, 'script', 'twitter-wjs'));
+
+        scope.$on('$destroy', function () {
+          angular.element(document.getElementById(idScript)).remove();
+        });
+      }
+    };
+  }).directive('rcSocialNetworkGooglePlus', function () {
+
+    return {
+      link: function (scope, element) {
+
+        window.___gcfg = {lang: 'fr'};
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.defer = true;
+        po.src = 'https://apis.google.com/js/platform.js';
+        po.innerHTML = '{lang: "fr"}';
+        var s = element[0];
+        s.parentNode.insertBefore(po, s);
+      }
+    };
+  }).directive('rcSocialNetworkPinterest', function () {
+
+    return {
+      link: function (scope, element) {
+        window.receiveCount = function (data) {
+          return data;
+        };
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = '//assets.pinterest.com/js/pinit.js';
+        po.innerHTML = '{lang: "fr"}';
+        var s = element[0];
+        s.parentNode.insertBefore(po, s);
+      }
+    };
+  });
